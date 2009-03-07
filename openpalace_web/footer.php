71ec28948566a62d06c5354f6245d06d2378c2cf
Brian McKelvey <theturtle32@gmail.com>
2009-03-07 00:36:21 +0000

    Updating to php.

---
+++ openpalace_web/footer.php
@@ -0,0 +1,21 @@
+      </div>
+    </div>
+    <div id="footer">
+      <div class="widthConstraint">
+        <div id="copyright">
+          Copyright © 2009 Brian McKelvey
+        </div>
+      </div>
+    </div>
+
+    <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+    try {
+    var pageTracker = _gat._getTracker("UA-7700604-1");
+    pageTracker._trackPageview();
+    } catch(err) {}</script>
+  </body>
+</html>
---
+++ openpalace_web/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+        "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <title>OpenPalace - Flash-based 2d Avatar Chat</title>
+    <link rel="stylesheet" href="stylesheet.css" type="text/css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body class="standards">
+    
+    <div id="header">
+      <div class="widthConstraint">
+        <h1>OpenPalace</h1>
+        <h2>Catchy tagline goes here.</h2>
+        <ul id="navigation">
+          <li><a href="/comingsoon.html">Get OpenPalace</a></li>
+          <li><a href="/comingsoon.html">Get the Source</a></li>
+          <li><a href="/trac/newticket">Report Bug</a></li>
+          <li><a href="/comingsoon.html">Documentation</a></li>
+          <li><a href="/comingsoon.html">Contribute</a></li>
+          <li><a href="/comingsoon.html">Contact</a></li>
+          <li><a href="/comingsoon.html">About</a></li>
+        </ul>
+      </div>
+    </div>
+    
+    <div id="mainContent">
+      <div class="widthConstraint">
